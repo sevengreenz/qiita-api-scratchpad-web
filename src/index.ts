a567405706cbf0b59b5113c4d6e8c4fc74c702d1
sevengreenz <banchikichi+sevengreenz@gmail.com>
2018-03-27 01:14:32 +0900

    add header にタイトル、説明、キーワード追加

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify';
+import Meta from 'vue-meta';
 import App from './presentation/App.vue';
 
 Vue.use(Vuetify);
+Vue.use(Meta);
 
 const vm = new App();
 vm.$mount('#app');
