1422e2030d5647b20cb3a24652f52cb8d448ed21
sevengreenz <banchikichi+sevengreenz@gmail.com>
2018-05-29 02:06:53 +0900

    modify 通信処理を interceptor を使って共通化

--- src/data/api/http-client-factory.ts
+++ src/data/api/http-client-factory.ts
@@ -1,7 +1,29 @@
 import axios, { AxiosRequestConfig } from 'axios';
+import ServerError from '../errors/server-error';
+import errorFactory from '../errors/error-factory';
 
-const createHttpClient = (config: AxiosRequestConfig) => axios.create(config);
+const createHttpClient = (config: AxiosRequestConfig) => {
+  const instance = axios.create(config);
+  instance.interceptors.response.use(
+    response => {
+      console.log(response);
+
+      if (response.data.errors !== undefined) {
+        errorFactory.throwError(response);
+      }
+
+      return response;
+    },
+    error => {
+      console.log(error);
+
+      throw new ServerError(error.message);
+    }
+  );
+
+  return instance;
+};
 
 export default {
-  createHttpClient,
+  createHttpClient
 };
--- src/data/repositories/data-stores/qiita/cloud-qiita-data-store.ts
+++ src/data/repositories/data-stores/qiita/cloud-qiita-data-store.ts
@@ -1,62 +1,38 @@
 import { IQiitaDataStore } from './qiita-data-store-interface';
 import { AxiosResponse } from 'axios';
 import { IApiResponse } from '../../../../domain/qiita';
-import errorFactory from '../../../errors/error-factory';
 import tokenRepository from '../../token-repository';
 import httpClientFactory from '../../../api/http-client-factory';
 import uuid from 'uuid/v4';
 
-const cloudQiitaDataStore =
-  (): IQiitaDataStore => {
-    const httpClient = httpClientFactory.createHttpClient({
-      baseURL: process.env.BASE_API_URL,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${tokenRepository.find() || ''}`,
-      },
-    });
+const cloudQiitaDataStore = (): IQiitaDataStore => {
+  const httpClient = httpClientFactory.createHttpClient({
+    baseURL: process.env.BASE_API_URL,
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${tokenRepository.find() || ''}`
+    }
+  });
 
-    return {
-      execute: async (method, url, params): Promise<IApiResponse> => {
-        const result: IApiResponse = await httpClient.post('/rpc', {
+  return {
+    execute: async (method, url, params): Promise<IApiResponse> => {
+      const result: IApiResponse = await httpClient
+        .post('/rpc', {
           jsonrpc: '2.0',
           method: 'executeApi',
           params: { method, url, params },
-          id: uuid(),
+          id: uuid()
         })
-          .then((response: AxiosResponse) => {
-            console.log(response);
-            if (response.data.errors !== undefined) {
-              if (response.data.errors.code === -32604) return Promise.reject(response);
-              return Promise.resolve({
-                headers: [],
-                data: response.data.errors.message,
-              });
-            }
+        .then((response: AxiosResponse) => {
+          return Promise.resolve({
+            headers: response.data.result.headers,
+            data: response.data.result.data
+          });
+        });
 
-            return Promise.resolve({
-              headers: response.data.result.headers,
-              data: response.data.result.data,
-            });
-          })
-          // .catch((error: AxiosError) => {
-          //   console.log(error);
-          //   if (error.response === undefined) return Promise.reject(error);
-
-          //   if (error.response.data.errors.code === -32604) return Promise.reject(error);
-
-          //   // 401 以外は正常として扱う
-          //   return Promise.resolve({
-          //     headers: error.response.headers,
-          //     data: error.response.data,
-          //   });
-          // })
-          .catch(errorFactory.throwError);
-
-        return result;
-      },
-
-    };
+      return result;
+    }
   };
+};
 
 export default cloudQiitaDataStore;
